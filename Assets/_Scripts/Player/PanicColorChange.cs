d92cf7d5ddb2bb204e16f5e3c104e7a34fc0323b
Saher Anwar Ziauddin <saher.ziauddin@dal.ca>
2024-09-14 18:34:58 -0300

    Completed light reflecting changes when as panic increases

--- Assets/_Scripts/Player/PanicColorChange.cs
+++ Assets/_Scripts/Player/PanicColorChange.cs
@@ -1,11 +1,20 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class PanicColorChange : MonoBehaviour
 {
+    [SerializeField] Light2D light2D;
     [SerializeField] Color calmColor;
     [SerializeField] Color panicColor;
+    [SerializeField] float maxIntensity;
+    [SerializeField] float maxOuterRadius;
+    [SerializeField] float maxInnerRadius;
 
     PanicBar panicBar;
+
+    private void Awake() {
+        light2D = GetComponent<Light2D>();
+    }
 
     private void Start() {
         panicBar = SpawnManager.Instance.panicBar;
@@ -13,6 +22,11 @@ public class PanicColorChange : MonoBehaviour
 
     void Update()
     {
-        panicBar.panicBarFill.color = Color.Lerp(calmColor, panicColor, panicBar.panicBarFill.fillAmount);
+        float currFillAmt = panicBar.panicBarFill.fillAmount;
+        panicBar.panicBarFill.color = Color.Lerp(calmColor, panicColor, currFillAmt);
+        light2D.color = Color.Lerp(calmColor, panicColor, currFillAmt);
+        light2D.intensity = Mathf.Lerp(maxIntensity, 0, currFillAmt);
+        light2D.pointLightOuterRadius = Mathf.Lerp(maxOuterRadius, 0, currFillAmt);
+        light2D.pointLightInnerRadius = Mathf.Lerp(maxInnerRadius, 0, currFillAmt);
     }
 }
